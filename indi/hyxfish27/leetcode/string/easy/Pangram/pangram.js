debb5cecdb072f6ace72be219cf8dd6901b02419
hyxfish27 <avicky1515@gmail.com>
2022-10-17 17:29:01 +0800

    feat: Pangram practice

---
+++ indi/hyxfish27/leetcode/string/easy/Pangram/pangram.js
@@ -0,0 +1,22 @@
+/**
+ * @author hyxfish27
+ * @param {string} sentence
+ * @returns {boolean} result
+ * @desc 檢查 sentence 是否為 pangram 語句
+ */
+export const pangram = (sentence) => {
+  if (sentence.length >= 1000) {
+    return false
+  }
+
+  const alphabet = []
+  for (let i = 0; i < 26; i++) {
+    alphabet[i] = false
+  }
+
+  for (let i = 0; i < sentence.length; i++) {
+    alphabet[sentence.charCodeAt(i) - 97] = true
+  }
+
+  return alphabet.every(char => char === true)
+}
