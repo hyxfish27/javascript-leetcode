0fcab14864b446c01e81575e7f9f420f5db9e5bd
hyxfish27 <avicky1515@gmail.com>
2022-10-17 17:20:52 +0800

    test: Pangram unit-test

---
+++ indi/hyxfish27/tests/leetcode/string/easy/pangram.spec.js
@@ -0,0 +1,31 @@
+import { pangram } from '../../../../leetcode/string/easy/Pangram/pangram'
+import { describe, it, expect } from 'vitest'
+
+const pangramCases = [
+  'the quick brown fox jumps over a lazy dog.',
+  'two driven jocks help fax my big quiz.'
+]
+
+const nonPangramCases = [
+  'abcdefghijklmnopq', // 沒有全部字母
+  'leetcode'
+]
+
+describe('Pangram Test Cases', () => {
+  it('should pass if every alphabet exists in the sentence', () => {
+    pangramCases.forEach(i => expect(pangram(i)).toBe(true))
+  })
+
+  it('should NOT pass if the sentence does not include all alphabet', () => {
+    expect(pangram(nonPangramCases[0])).toBe(false)
+  })
+
+  it('should NOT pass if the length of the sentence is over 1000', () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let longStr = ''
+    for (let i = 0; i < 1000; i++) {
+      longStr += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    expect(pangram(longStr)).toBe(false)
+  })
+})
