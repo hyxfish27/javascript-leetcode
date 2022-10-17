5f2b21b56739e620c71da17778144d71155fa148
hyxfish27 <avicky1515@gmail.com>
2022-10-17 15:27:53 +0800

    test: 引入 vitest 測試工具

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'indi/hyxfish27'
+  root: 'indi/hyxfish27',
+  test: {
+    include: ['**/*.spec.[t|j]s'],
+    exclude: ['**/node_modules/**', '**/dist/**'],
+    environment: 'node'
+  }
 })
